497df9079c6efa98f4eeaee385fed5a875b0c949
Berke Sayın <sayinberkesayin@gmail.com>
2022-12-10 14:46:49 +0300

    Install express. Start developing backend. Create first routes with express.js.

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+// express'i kullanmak için getirelim
+const express = require("express");
+
+// app değişkeni ile express'i initialize edelim
+const app = express();
+
+// importing products
+const products = require("./data/products");
+
+// respond with "API is running" when a GET request is made to the homepage
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+// Burada localhost:5000'e gittiğimizde "API is running..." yazdırıldığını görürüz.
+
+// creating the route to get all the products as json
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+// creating the route to get specific product by product._id as json
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(5000, console.log("Server running on port 5000"));
