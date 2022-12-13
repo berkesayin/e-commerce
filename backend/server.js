c860607cd6b2e4d0508d5a9dff17dc2d704e4da6
Berke Sayın <sayinberkesayin@gmail.com>
2022-12-13 21:44:45 +0300

    Install dotenv package from npm. Create .env file for environment variables.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 // express'i kullanmak için getirelim
 const express = require("express");
 
-// app değişkeni ile express'i initialize edelim
-const app = express();
+const dotenv = require("dotenv");
 
 // importing products
 const products = require("./data/products");
+
+dotenv.config();
+
+// app değişkeni ile express'i initialize edelim
+const app = express();
 
 // respond with "API is running" when a GET request is made to the homepage
 app.get("/", (req, res) => {
@@ -24,4 +28,6 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT} `));
