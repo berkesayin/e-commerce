c1fd5f88e85b1c0677c26714e030ab72ecab3fb9
Berke Sayın <sayinberkesayin@gmail.com>
2023-02-14 22:07:53 +0300

    Add custom error messages to the responses for requests(custom error handling).

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-
-import express from 'express';
-import dotenv from 'dotenv';
-import products from './data/products.js';
-import connectDB from './config/db.js';
-import colors from 'colors';
-import productRoutes from './routes/productRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+import connectDB from "./config/db.js";
+import colors from "colors";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -13,15 +13,25 @@ connectDB();
 // app değişkeni ile express'i initialize edelim
 const app = express();
 
+
 // respond with "API is running" when a GET request is made to the homepage
 app.get("/", (req, res) => {
   res.send("Hi! API is running...");
 });
 // Burada localhost:5000'e gittiğimizde "API is running..." yazdırıldığını görürüz.
 
-app.use('/api/products/', productRoutes);
+app.use("/api/products/", productRoutes);
 
-const PORT = process.env.PORT || 5000
+app.use(notFound)
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold));
+app.use(errorHandler)
 
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
+  )
+);
