b9f465e8d4117d58bb8c2a550d1837677a02c149
Berke Sayın <sayinberkesayin@gmail.com>
2023-03-01 22:56:50 +0300

    Create add to cart functionality.

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,16 +1,27 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM } from "../constants/cartConstants";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
+      
       const item = action.payload;
-      const existItem = state.cartItems.find((x) => x.product === item.product);
+      
+      /*
+      console.log('Reducer file: Item: ', item);
+      console.log('Reducer file: item.product: ', item.product);
+      console.log("Reducer file: state.cartItems: ", state.cartItems);
+      console.log('Reducer file: action: ', action);
+      console.log('Reducer file action.type: ', action.type);
+      console.log('Reducer file: action.payload: ', action.payload);
+      */
+            
+      const existItem = state.cartItems.find((x) => x.product === item.product)
 
       if (existItem) {
-        return {
+        return {  
           ...state,
           cartItems: state.cartItems.map((x) =>
-            x.product === existItem.product ? item : x
+            x.product === existItem.product ? item : x  
           ),
         };
       } else {
@@ -20,6 +31,6 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
         };
       }
     default:
-      return state;
-  }
+      return state;  
+   }
 };
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,22 +1,84 @@
-import React from 'react'
-// import { useParams, useLocation, useSearchParams } from 'react-router-dom'
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Link,
+  useParams,
+  useLocation,
+  useSearchParams,
+} from "react-router-dom";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import Message from "../components/Message";
+import { addToCart } from "../actions/cartActions";
+
 
 const CartScreen = () => {
+  
+  /* => stackoverflow
+  const { id } = useParams();
+  const { search } = useLocation();
+  const [searchParams] = useSearchParams();
+  const productID = id;
+  const qty = search ? Number(search.split("=")[1]) : 1;
+  console.log({ productID, qty, qtyParam: Number(searchParams.get("qty")) });
+  */
 
-  // const { id } = useParams();
-  // const { search } = useLocation();
-  // const [searchParams] = useSearchParams();
 
-  // const productID = id;
-  // const qty = search ? Number(search.split("=")[1]) : 1;
+  /*  => video
+  const productId = match.params.id;
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+  const dispatch = useDispatch();
+  */
 
-  // console.log({ productID, qty, qtyParam: Number(searchParams.get("qty")) });
 
-  return (
-    <div>
-      Cart Screen Berke
-    </div>
-  )
-}
+  const { id } = useParams();
+  const productId = id;
 
-export default CartScreen
+  const { search } = useLocation();
+ 
+  /* 
+  const qty = search; 
+  Bu şekilde URL'de ürün id'sinden sonraki query params bilgisi alınır. Örneğin bir üründen 2 miktarını ü
+  seçip sepete eklediğimizde yönlendirilen URL'de /productId?qty=2 şeklinde gözükür. Console'da ise 
+  query params olan ?qty=2 yazdırılır. Yani search ?qty=2 bilgisini tutar. 
+  */
+  
+  const qty = search ? Number(search.split("=")[1]) : 1;
+  /* 
+  Burada ?qty=2 'yi = ile bölüp, eşittirin sağındaki number bilgisini yani 1.index'teki bilgiyi alıyoruz
+  Çünkü bize quantity miktarı lazım. 
+  */
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart)
+  const { cartItems } = cart
+
+  // console.log("Cart Items: ", cartItems);
+
+  
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
+  /*
+  console.log('CartScreen: Product Id: ', productId)
+  console.log('CartScreen: Quantity: ', qty)
+  console.log('CartScreen: Cart: ', cart)
+  console.log('CartScreen: Cart Items: ', cartItems)
+ */
+
+  return <div>Cart Screen Berke</div>;
+};
+
+export default CartScreen;
