28e7fd8433e9b3074f27cce965731e6a8b001afb
Berke Sayın <sayinberkesayin@gmail.com>
2023-03-07 22:47:14 +0300

    Add controllers methods for products.

---
+++ backend/controllers/productController.js
@@ -0,0 +1,33 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+const router = express.Router(); // express.js Router özelliği
+import Product from "../models/productModel.js";
+
+// @desc     Fetch all products
+// @route    GET /api/products
+// @access   Public route (No token needed)
+
+// creating the function to get all the products as json (we use at HomeScreen)
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+
+// @desc     Fetch single product
+// @route    GET /api/products/:id
+// @access   Public
+
+// creating the function to get specific product by product._id as json (we use at ProductScreen)
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found!");
+  }
+});
+
+export { getProducts, getProductById };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,15 @@
 import express from "express";
 const router = express.Router(); // express.js Router özelliği
+import { getProducts, getProductById } from '../controllers/productController.js'
 
-import Product from "../models/productModel.js";
-import asyncHandler from "express-async-handler";
-
-// @desc     Fetch all products 
-// @route    GET /api/products
-// @access   Public route (No token needed)
 
 // creating the route to get all the products as json (we use at HomeScreen)
-router.get("/", asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+// router.get("/", getProducts);
+router.route('/').get(getProducts);
 
-// @desc     Fetch all product
-// @route    GET /api/products/:id
-// @access   Public 
 
 // creating the route to get specific product by product._id as json (we use at ProductScreen)
-router.get("/:id", asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
+router.route('/:id').get(getProductById);
 
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404)
-    throw new Error('Product not found!')
-  }
-}));
 
 export default router;  
