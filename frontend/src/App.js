6e671870fdf527ae85e59e3a3bf81588c00aa7f7
Berke Sayın <sayinberkesayin@gmail.com>
2023-03-24 22:51:05 +0300

    Add profile screen and get user details.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -29,6 +30,7 @@ const App = () => {
             {/* <Route path='/cart/:id?qty=:qty' element={<CartScreen />} /> */}
             <Route path='/login' element={<LoginScreen />} />
             <Route path='/register' element={<RegisterScreen />} />
+            <Route path='/profile' element={<ProfileScreen />} />
           </Routes>
         </Container>
       </main>
