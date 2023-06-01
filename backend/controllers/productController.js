a2f71f1840176be926645b7ddd8eaf6b10b98706
Berke Sayın <sayinberkesayin@gmail.com>
2023-06-01 18:05:04 +0300

    Add admin functionality for delete product for both backend and frontend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -13,7 +13,6 @@ const getProducts = asyncHandler(async (req, res) => {
   res.json(products);
 });
 
-
 // @desc     Fetch single product
 // @route    GET /api/products/:id
 // @access   Public
@@ -30,4 +29,20 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+// @desc     Delete a product
+// @route    DELETE /api/products/:id
+// @access   Private/Admin
+
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product Removed" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found!");
+  }
+});
+
+export { getProducts, getProductById, deleteProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,13 +3,17 @@ const router = express.Router(); // express.js Router özelliği
 import {
   getProducts,
   getProductById,
+  deleteProduct
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
 
 // creating the route to get all the products as json (we use at HomeScreen)
 // router.get("/", getProducts);
 router.route("/").get(getProducts);
 
 // creating the route to get specific product by product._id as json (we use at ProductScreen)
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
+ 
